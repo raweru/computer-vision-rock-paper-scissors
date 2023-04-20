2db17b2b49529108950cebd3383b44afc6aba965
Dominik Dekleva <dominik.dekleva@hotmail.com>
2023-04-20 09:30:48 +0100

    get_prediction + 2nd model

--- camera_rps.py
+++ camera_rps.py
@@ -3,8 +3,7 @@ import numpy as np
 
 def get_prediction():
     class_names = ["Rock", "Paper", "Scissors", "Nothing"]
-    predicted_id = int(np.argmax(prediction, axis=-1))
-    predicted_class = class_names[predicted_id]
-    print(predicted_class)
+    predicted_class = np.argmax(prediction[0])
+    print("Predicted class: ", class_names[predicted_class])
 
 get_prediction()
