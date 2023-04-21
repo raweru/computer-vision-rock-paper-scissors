84c5863e37bd3a503e1da46db12812c1df850380
Dominik Dekleva <dominik.dekleva@hotmail.com>
2023-04-21 12:56:15 +0100

    working model_slovenia

--- camera_rps.py
+++ camera_rps.py
@@ -2,8 +2,13 @@ from RPS_Template import prediction
 import numpy as np
 
 def get_prediction():
-    class_names = ["Rock", "Paper", "Scissors", "Nothing"]
-    predicted_class = np.argmax(prediction[0])
-    print("Predicted class: ", class_names[predicted_class])
+    class_names = open("labels.txt", "r").readlines()
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
 get_prediction()
